19230fc15270eb77e0d53c1ebd720b938cb72558
Kostas Loukas <kostas99loukas@gmail.com>
2023-08-24 14:02:13 +0300

    update app

--- src/Arguments.py
+++ src/Arguments.py
@@ -1,5 +1,5 @@
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
 from Utility_Functions import *
 
 
@@ -11,27 +11,32 @@ class Arguments:
     product_id = None
     start_date = None
     expire_date = None
+    hash_type = None
 
     # FIXME: ParseArguments
-
     def ParseArguments(self) -> None:
 
         self.parser = argparse.ArgumentParser(prog='SignaScribe')
 
         # define manifest fields
-        # self.parser.add_argument('-mi', '--manifest_id',
-        #                          type=int, required=True, help="Manifest ID")
-        # self.parser.add_argument('-f', '--file', type=argparse.FileType("rw"), required=True,
-        #                          help="The file that will be signed")
-        # self.parser.add_argument(
-        #     '-pi', '--product_id', type=int, required=True)
-        # self.parser.add_argument('-vi', '--vendor_id', type=int, required=True)
-        # self.parser.add_argument('-sa', '--sign_algorithm',
-        #                          type=int, required=True, help="")
-        self.parser.add_argument('-sd', '--start_date', type=str, default=datetime.now().strftime('%d-%m-%Y'),
-                                 help="The starting date of image validation in dd-mm-yy format")
+        self.parser.add_argument('-mi', '--manifest_id',
+                                 type=int, choices=[0], required=True, help="Manifest ID")
+        self.parser.add_argument('-f', '--file', type=argparse.FileType("rb"), required=True,
+                                 help="The file that will be signed")
+        self.parser.add_argument('-ht', '--hash_type', type=str, choices=["sha256"], required=True,
+                                 help="The hash algorithm that will be used")
         self.parser.add_argument(
-            '-ed', '--expire_date', type=str, default=datetime.now().strftime('%d-%m-%Y'), help="The expiration date of image validation in dd-mm-yy format")
+            '-pi', '--product_id', type=str, required=True, help="The device's product id")
+        self.parser.add_argument(
+            '-vi', '--vendor_id', type=str, required=True, help="The device's vendor id")
+
+        # TODO: provide the options for sign algorithm
+        self.parser.add_argument('-sa', '--sign_algorithm',
+                                 type=int, required=True, choices=[0], help="Choose which digital sign algorithm you want to use")
+        self.parser.add_argument('-sd', '--start_date', type=str, default=datetime.now().strftime('%d-%m-%Y'),
+                                 help="The starting date of file validation in dd-mm-yy format")
+        self.parser.add_argument(
+            '-ed', '--expire_date', type=str, help="The expiration date of file validation in dd-mm-yy format")
 
         # parse arguments
         arguments = self.parser.parse_args()
@@ -44,30 +49,51 @@ class Arguments:
 
         try:
 
+            # If an expiry date is not provided, then set it
+            # to expire 10 days after the start date.
             if (arguments.expire_date == None):
-                arguments.expire_date = datetime.strptime(
-                    arguments.expire_date, '%d-%m-%Y').year = 2026
+                arguments.expire_date = (datetime.strptime(
+                    arguments.start_date, '%d-%m-%Y') + timedelta(days=10)).strftime('%d-%m-%Y')
 
+            # start date
             self.start_date = datetime.strptime(
                 arguments.start_date, '%d-%m-%Y').strftime('%d-%m-%Y')
 
+            # expire date
             self.expire_date = datetime.strptime(
                 arguments.expire_date, '%d-%m-%Y').strftime('%d-%m-%Y')
 
+            # if start date is older than expire date then throw error
             if (datetime.strptime(self.start_date, '%d-%m-%Y') > datetime.strptime(self.expire_date, '%d-%m-%Y')):
                 ERROR("Start date > Expire date")
+
+            # product id
+            self.product_id = int(arguments.product_id, 16)
+
+            # vendor id
+            self.vendor_id = int(arguments.vendor_id, 16)
+
+            # manifest id
+            self.manifest_id = arguments.manifest_id
+
+            # read file
+            self.file = arguments.file.read()
+
+            # hash type
+            self.hash_type = arguments.hash_type
 
         except ValueError as ex:
             ERROR(ex)
 
-        # assert (self.file != None and
-        #         self.manifest_id != None and
-        #         self.vendor_id != None and
-        #         self.product_id != None and
-        #         self.file != None and
-        #         self.start_date != None and
-        #         self.expire_date != None)
+        assert (self.file != None and
+                self.manifest_id != None and
+                self.vendor_id != None and
+                self.product_id != None and
+                self.file != None and
+                self.start_date != None and
+                self.expire_date != None and
+                self.hash_type != None)
         pass
 
     def __str__(self) -> str:
-        return f"Argument object: start_date={self.start_date}, expire_date={self.expire_date}"
+        return f"Argument object: manifest id={self.manifest_id} , vendor id={self.vendor_id} , product id={self.product_id} , start_date={self.start_date} , expire_date={self.expire_date} , hash type={self.hash_type} "
